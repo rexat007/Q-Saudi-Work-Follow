084123f9423bcda248828d8574defd2ee738a644
rexat007 <Raharaya2002@gmail.com>
2026-09-10 17:07:43 +0300

    feat(i18n): wire provider and language selector

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './firebase/authContext';
+import { I18nProvider } from './i18n';
+import { LanguageSelector } from './components/i18n/LanguageSelector';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <I18nProvider>
+      <AuthProvider>
+        <LanguageSelector />
+        <App />
+      </AuthProvider>
+    </I18nProvider>
   </StrictMode>,
 );
